cb4fd9315de6a3a81655d03400115c581a0cf94e
mymhj <290502450@qq.com>
2017-04-21 17:01:26 +0800

    清除

--- TESTCODE/001/Debug/注册.bat
+++
@@ -1,5 +0,0 @@
-@echo 开始注册
-copy dll %windir%\system32\
-regsvr32 %windir%\system32\dll /s
-@echo dll注册成功
-@pause
--- TESTCODE/001/GPothka.MainWin/importData.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "stdafx.h"
-
-bool importData( char* fileName );
-
